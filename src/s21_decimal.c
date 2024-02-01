1b1a438bc4ee552b9e4888c23aecb42d77b1867f
Marlon Antionette <marlonan@ob-g6.nsk.21-school.ru>
2024-02-01 18:50:15 +0700

    add started

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -8,6 +8,78 @@
 
 
 
+int s21_get_sign(s21_decimal number) {
+        int sign = number.bits[SCALE] & MINUS;
+        return sign;
+    }
+
+void s21_set_sign(s21_decimal *number, int sign) {
+    if(sign == 1){
+        number->bits[SCALE] |= MINUS;
+    }
+    if(sign == 0){
+        number->bits[SCALE] &= ~MINUS;
+    }
+}
+
+void s21_set_bit(int *number, int bit_position, int bit) {
+    int mask = 1; 
+    mask <<= bit_position;
+
+    if(bit == 0) {
+        mask = ~mask;
+        *number &= mask;
+    }
+
+    if(bit == 1) {
+        *number |= mask;
+    }
+}
+
+
+
+// PURE SHITCODE  LOL
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result){
+    // s21_big_decimal value_1_b = s21_decimal_to_big(value1);
+    // s21_big_decimal value_2_b = s21_decimal_to_big(value2);
+    // s21_big_decimal value_3_b = s21_decimal_to_big(result);
+    int flag = 0;
+    for(int i =0;i<7;i++){
+    for(int j = 0; j<64;i++){
+
+        if((value_1_b >>j & first) == 0 && (value_2_b>>j & first) == 0 && flag = 0) 
+        {
+            s21_set_bit(value_3_b,j,0); 
+        }
+        else if ((value_1_b >>j & first) == 0 && (value_2_b>>j & first) == 0 && flag = 1) 
+        { 
+            s21_set_bit(value_3_b,j,0); flag = 0;
+        }
+
+
+
+        if( (value_1_b >>j & first) == 1 && (value_2_b>>j & first) == 0 && flag = 0) || ((value_1_b & first) == 0 && (value_1_b & first) == 1 && flag = 0) 
+        {
+            s21_set_bit(value_3_b,j,1);
+        }
+
+        else if ((value_1_b >>j & first) == 1 && (value_2_b>>j & first) == 0 && flag = 1) || ((value_1_b & first) == 0 && (value_1_b & first) == 1 && flag = 1)
+        {
+           s21_set_bit(value_3_b,j,0); 
+        }
+
+
+
+        if((value_1_b >>j & first) == 1 && (value_2_b>>j & first) == 1) 
+        {
+            s21_set_bit(value_3_b,j,0); flag = 1;
+        }
+    
+
+    }
+    }
+}
 
 
 
@@ -21,7 +93,7 @@ int get_scale(s21_decimal s21_decimal){
  return scale;
 //  printf("%d",scale);
 }
-// --------------------- set scale -----------------------
+// --------------------- set scale for decimal-----------------------
 
 // s21_decimal decimal1 =  {{0x00000000, 0x00000000, 0x00000000, 0x140000}};
 // int scale = decimal1.bits[3] & SCALEBITE;
@@ -32,6 +104,7 @@ int get_scale(s21_decimal s21_decimal){
 // scale = decimal1.bits[3] & SCALEBITE;
 // printf("\n%d", scale >> 16); 
 // // printf("%d", new_scale); 
+
 int main(){
 // --------------------- convert to big decimal -----------------------
 
