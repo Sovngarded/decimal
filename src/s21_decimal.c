ed25251e3b6e6fd3d53c971ebad9d2431b0345f2
Marlon Antionette <marlonan@ob-g6.nsk.21-school.ru>
2024-02-01 14:46:28 +0700

    get and set scale done

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -11,7 +11,31 @@
 
 
 int main(){
- s21_decimal decimal1 = {{0x00FF0000, 0, 0, 0}};
- int scale = decimal1.bits[4] & SCALEBITE;
- printf("%d",scale);
+
+// --------------------- get scale -----------------------
+//  s21_decimal decimal1 = {{0x00190000, 0, 0, 0}};
+//  int scale = decimal1.bits[0] & SCALEBITE;
+// //  int new = scale >>16;
+//  scale >>= 16;
+//  printf("%d",scale);
+
+// --------------------- set scale -----------------------
+
+s21_decimal decimal1 =  {{0x00000000, 0x00000000, 0x00000000, 0x140000}};
+int scale = decimal1.bits[3] & SCALEBITE;
+printf("%d", scale >> 16); 
+int new_scale = 0x001C;
+decimal1.bits[3] &= MINUS;
+decimal1.bits[3] |= ((new_scale<<16) & SCALEBITE);
+scale = decimal1.bits[3] & SCALEBITE;
+printf("\n%d", scale >> 16); 
+// printf("%d", new_scale); 
+
+
+
+
+
+
+
+
 }
